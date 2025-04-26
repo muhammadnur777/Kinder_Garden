1bc7efe3a577b9ad1e2eb0ebe672e2acb0fcaec3
unknown <muhammadnuryusufjonov31@gmail.com>
2025-04-26 17:45:21 +0500

    models edited

--- kinder_garden/admin.py
+++ kinder_garden/admin.py
@@ -1,14 +1,12 @@
 from django.contrib import admin
 from .models import Classes, Banner, PhotoGallery
 
-@admin.site.register(Banner)
+# @admin.site.register(Banner)
 
 
-
-
-
-
-
+@admin.register(Banner)
+class BannerAdmin(admin.ModelAdmin):
+    list_display = ('banner_image',)
 
 @admin.register(Classes)
 class SchoolClassesAdmin(admin.ModelAdmin):
